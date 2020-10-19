5d5345478f6a89ef5131bde086c68d98c1146d8b
Mick Semb Wever <mck@apache.org>
2020-10-19 21:36:23 +0200

    Rename master branch to trunk in cassandra-builds (CASSANDRA-16163)

--- jenkins-dsl/cassandra_job_dsl_seed.groovy
+++ jenkins-dsl/cassandra_job_dsl_seed.groovy
@@ -34,7 +34,7 @@ def buildsRepo = "https://gitbox.apache.org/repos/asf/cassandra-builds.git"
 if(binding.hasVariable("CASSANDRA_BUILDS_GIT_URL")) {
     buildsRepo = "${CASSANDRA_BUILDS_GIT_URL}"
 }
-def buildsBranch = "master"
+def buildsBranch = "trunk"
 if(binding.hasVariable("CASSANDRA_BUILDS_BRANCH")) {
     buildsBranch = "${CASSANDRA_BUILDS_BRANCH}"
 }
