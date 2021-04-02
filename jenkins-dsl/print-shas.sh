afb103d133a0008d8244d973c0801aad5deedba6
Mick Semb Wever <mck@apache.org>
2021-04-02 12:27:04 +0200

    ninja-fix sha printing

--- jenkins-dsl/print-shas.sh
+++ jenkins-dsl/print-shas.sh
@@ -4,10 +4,10 @@
 # If they all match then print just them, else print every job's used SHA
 #
 
-if [ $(grep ") cassandra" *.head | sort -k4 | uniq -f3 | sed -n '$=') = "3" ]; then
+if [ $(find . -type f -name "*.head" -exec cat {} \; | grep ") cassandra" | sort -k4 | uniq -f3 | sed -n '$=') = "3" ]; then
   echo "The folllowing SHAs were consistently used in all jobs in the pipeline…"
-  grep ") cassandra" *.head | awk '{$1=$2=$3=""; print $0}' | sort -u
+  find . -type f -name "*.head" -exec cat {} \; | grep ") cassandra" | awk '{$1=$2=$3=""; print $0}' | sort -u
 else
   echo "Different SHAs were used in different jobs in the pipeline. Printing everything…"
-  grep ") cassandra" *.head
+  find . -type f -name "*.head" -exec cat {} \;
 fi
