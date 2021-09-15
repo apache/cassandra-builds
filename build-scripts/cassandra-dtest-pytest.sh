86db69615e77c095d54105869fa1344b7735a863
Bereng <berenguerblasi@gmail.com>
2021-09-15 08:54:13 +0200

    Bug

--- build-scripts/cassandra-dtest-pytest.sh
+++ build-scripts/cassandra-dtest-pytest.sh
@@ -87,7 +87,7 @@ fi
 SPLIT_TESTS=""
 if [ "x${DTEST_SPLIT_CHUNK}" != "x" ] ; then
     ./run_dtests.py --cassandra-dir=$CASSANDRA_DIR ${DTEST_ARGS} --dtest-print-tests-only --dtest-print-tests-output=${WORKSPACE}/reuseClusterTests.txt --pytest-options "-m reuse_cluster" 2>&1 > ${WORKSPACE}/test_stdout_reuse.txt
-    ./run_dtests.py --cassandra-dir=$CASSANDRA_DIR ${DTEST_ARGS} --dtest-print-tests-only --dtest-print-tests-output=${WORKSPACE}/allClusterTests.txt --pytest-options  2>&1 > ${WORKSPACE}/test_stdout_all.txt
+    ./run_dtests.py --cassandra-dir=$CASSANDRA_DIR ${DTEST_ARGS} --dtest-print-tests-only --dtest-print-tests-output=${WORKSPACE}/allClusterTests.txt 2>&1 > ${WORKSPACE}/test_stdout_all.txt
     grep -vf ${WORKSPACE}/reuseClusterTests.txt ${WORKSPACE}/allClusterTests.txt > ${WORKSPACE}/renewClusterTests.txt
     CHUNK=`echo ${DTEST_SPLIT_CHUNK}|cut -d "/" -f 1`
     SPLITS=`echo ${DTEST_SPLIT_CHUNK}|cut -d "/" -f 2`
