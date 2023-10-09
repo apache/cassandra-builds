8d11eeabd7825e63f883c68ffa2b67a02c1bed1c
Mick Semb Wever <mck@apache.org>
2023-10-09 13:04:00 +0200

    ninja-fix – warn instead of failing if the pipeline build fails generating the unified xml test report file fails (xslt xerces easily OOMs)
    ref: https://the-asf.slack.com/archives/CK23JSY2K/p1696599019480519

--- build-scripts/cassandra-test-report.sh
+++ build-scripts/cassandra-test-report.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 # merge all the unit test xml files into one TESTS-TestSuites.xml file (which will get archived at nightlies.a.o)
-ant -f ./cassandra-builds/build-scripts/cassandra-test-report.xml
+ant -quiet -silent -f ./cassandra-builds/build-scripts/cassandra-test-report.xml || ( echo "WARN failed to unify test results" && touch TESTS-TestSuites.xml )
